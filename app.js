a4cc564437636f89664f24f70658c3b0c52a4d58
aditaryar <adityarya189@gmail.com>
2026-04-23 15:40:19 +0700

    update

--- app.js
+++ app.js
@@ -5,38 +5,39 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import uploadRoutes from "./src/routes/uploadRoutes.js";
-import initDB  from "./src/db/database.js";
-
-export let db;
-
-initDB().then((database) => {
-  db = database;
-  console.log("🔥 Database ready");
-});
+import initDB from "./src/db/database.js";
+import { createTable } from "./src/models/orderModel.js";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-];
+const startServer = async () => {
+  try {
+    const db = await initDB();
+    await createTable(db);
 
-app.use(cors({
-  origin: "*"
-}));
+    console.log("🔥 Database ready");
 
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("❌ Failed to start server:", err);
+  }
+};
+
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-import { fileURLToPath } from "url";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use("/uploads", express.static(path.join("src/uploads")));
 
-// =======================
-// ✅ SWAGGER CONFIG
-// =======================
+app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
+
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
@@ -46,15 +47,12 @@ const swaggerOptions = {
       description: "API untuk sistem TokSort",
     },
     servers: [
-      // {
-      //   url: "http://localhost:3000",
-      // },
       {
         url: "https://toksort-backend-production.up.railway.app",
       },
     ],
   },
-  apis: ["./src/routes/*.js"], // 🔥 baca komentar swagger di routes
+  apis: ["./src/routes/*.js"],
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
@@ -63,8 +61,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api", uploadRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-  console.log(`📚 Swagger docs at http://localhost:${PORT}/api-docs`);
-});
+startServer();
